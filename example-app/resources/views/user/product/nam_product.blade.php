70e73400838b68cc06cd076fed2e80c111fd7c64
phuongVu12 <phuongvune2004@gmail.com>
2024-10-13 14:43:59 +0700

    update index

--- example-app/resources/views/user/product/nam_product.blade.php
+++ example-app/resources/views/user/product/nam_product.blade.php
@@ -75,6 +75,97 @@
                         </li>
                     </ul>
                 </div>
+                <div class="search_left">
+                    <h5>Màu sắc</h5>
+                    <div class="search_color">
+                        <div class="checkbox_color">
+                            <input type="checkbox" name="color" id="color_xanhla">
+                            <label for="color_xanhla" >
+                                <div class="color -xanhla" style="background-color: bisque"></div>
+                                <span>Xanh Lá</span>
+                            </label>
+                        </div>
+                         <div class="checkbox_color">
+                            <input type="checkbox" name="color" id="color_den">
+                             <label for="color_den">
+                                <div class="color -den" style="background-color: rgb(97, 97, 97)"></div>
+                                <span>Đen</span>
+                            </label>
+                        </div>
+                         <div class="checkbox_color">
+                            <input type="checkbox" name="check3" id="color_xam">
+                             <label for="color_xam">
+                                <div class="color -xam" style="background-color: red"></div>
+                                <span>Xám</span>
+                            </label>
+                        </div>
+                         <div class="checkbox_color">
+                            <input type="checkbox" name="check3" id="color_trang">
+                             <label for="color_trang">
+                                <div class="color -trang" style="background-color: red"></div>
+                                <span>Trắng</span>
+                            </label>
+                        </div>
+                         <div class="checkbox_color">
+                            <input type="checkbox" name="check3" id="color_lam">
+                             <label for="color_lam">
+                                <div class="color -lam" style="background-color: red"></div>
+                                <span>Xanh Lam</span>
+                            </label>
+                        </div>
+                         <div class="checkbox_color">
+                            <input type="checkbox" name="check3" id="color_nau">
+                             <label for="color_nau">
+                                <div class="color -nau" style="background-color: red"></div>
+                                <span>Nâu</span>
+                            </label>
+                        </div>
+                         <div class="checkbox_color">
+                            <input type="checkbox" name="check3" id="color_be">
+                             <label for="color_be">
+                                <div class="color -be" style="background-color: red"></div>
+                                <span>Be</span>
+                            </label>
+                        </div>
+                         <div class="checkbox_color">
+                            <input type="checkbox" name="check3" id="color_reu">
+                             <label for="color_reu">
+                                <div class="color -reu" style="background-color: red"></div>
+                                <span>Xanh Rêu</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="search_left">
+                    <h5>Chất liệu</h5>
+                    <div class="check_search">
+                        <div class="chatlieu">
+                            <input type="checkbox" name="check" id="chatlieu">
+                            <label for="chatlieu"></label>
+                            <span>Vải Cotton</span>
+                        </div>
+                         <div class="chatlieu">
+                            <input type="checkbox" name="check2" id="chatlieu2">
+                            <label for="chatlieu2"></label>
+                            <span>Vải Excool</span>
+                        </div>
+                         <div class="chatlieu">
+                            <input type="checkbox" name="check3" id="chatlieu3">
+                            <label for="chatlieu3"></label>
+                            <span>Vải Modal (gỗ sôi)</span>
+                        </div>
+                        <div class="chatlieu">
+                            <input type="checkbox" name="check3" id="chatlieu4">
+                            <label for="chatlieu4"></label>
+                            <span>Vải Polyester tính năng</span>
+                        </div>
+                        <div class="chatlieu">
+                            <input type="checkbox" name="check3" id="chatlieu5">
+                            <label for="chatlieu5"></label>
+                            <span>Vải Recycle</span>
+                        </div>
+                    </div>
+                </div>
         </div>
         <div class="inline"></div>
         <div class="product_nam">
@@ -99,6 +190,42 @@
                     <p class="name">Áo giữ nhiệt Nam</p>
                 </div>
             </div>
+            <div class="loc_sp">
+                <div class="sp_hienco">
+                    <p>150 kết quả</p>
+                </div>
+                <div class="drop_loc">
+                    <span>SẮP XẾP THEO</span>
+                    <div class="dropdown_sp">
+                        <button class="dropdown_sp_button">
+                            Mặc định
+                            <i class="fa-solid fa-caret-down"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="sp_nam">
+                <div class="in_product">
+                    <div class="img_product">
+                        <img src="/backend/giaodien/img/product_nam/ao2.webp" class="home_product" alt="" >
+                        <img src="/backend/giaodien/img/product_nam/ao.webp" class="hover" alt="">
+                    </div>
+                    <p class="name">Áo giữ nhiệt Nam</p>
+                </div>
+                <div class="in_product">
+                    <div class="img_product">
+                        <img src="/backend/giaodien/img/product_nam/ao.webp" alt="">
+                    </div>
+                    <p class="name">Áo giữ nhiệt Nam</p>
+                </div>
+                <div class="in_product">
+                    <div class="img_product">
+                        <img src="/backend/giaodien/img/product_nam/ao.webp" alt="">
+                    </div>
+                    <p class="name">Áo giữ nhiệt Nam</p>
+                </div>
+            </div>
         </div>
     </div>
+
 
